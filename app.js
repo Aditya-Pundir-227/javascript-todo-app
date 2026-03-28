3192e78b30ec34cf78bc44bf031a79b0de4b6a61
Aditya Pundir <adityapundir22@gmail.com>
2026-03-28 23:05:24 +0530

    (add code)

---
+++ app.js
@@ -0,0 +1,39 @@
+let inp=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+
+btn.addEventListener('click',function(){
+let item=document.createElement("li");
+item.innerText=inp.value;
+
+let delbtn=document.createElement("button");
+delbtn.innerText="Delete";
+delbtn.classList.add("delete");
+
+item.appendChild(delbtn);
+ul.appendChild(item);
+inp.value="";
+})
+
+
+//! Without Event Delegation
+
+// let delbtns=document.querySelectorAll(".delete");
+// for(delbtn of delbtns){
+//     delbtn.addEventListener("click", function(){
+//         let par=delbtn.parentElement;
+//         console.log(par)
+//     })
+
+// }
+
+
+//! With Event Delegation
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem=event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+   
+})
